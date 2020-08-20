375dc2aef0e9a02f4d19c85249f25238067fbdf2
Aaron Brady <aleebrady@gmail.com>
2020-08-20 05:28:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,11 @@ def line(line)
   line = []
   if line.count == 0
   puts "The line is currently empty."
-  else line.each.with_index do |n, i|
-  puts "The line is currently: #{i+1}. #{n}"
-  binding.pry
+  else 
+    waiting = "The line is currently:"
+    line.each.with_index do |n, i|
+      waiting << "#{i+1}. #{name}"
 end
+puts waiting
 end
 end

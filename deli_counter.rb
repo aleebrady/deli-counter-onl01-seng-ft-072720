eef903d5e146db0d57b7c1b1753ad64e37a7dead
Aaron Brady <aleebrady@gmail.com>
2020-08-20 05:28:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 require 'pry'
 
-def line(line)
-  line = []
-  if line.count == 0
-  puts "The line is currently empty."
+def line(katz_deli)
+  line_order = []
+  if katz_deli.count == 0
+    puts "The line is currently empty."
   else 
     waiting = "The line is currently:"
-    line.each.with_index do |n, i|
-      waiting << "#{i+1}. #{name}"
-end
-puts waiting
-end
+    katz_deli.each.with_index do |name, index|
+      waiting << " #{index + 1}. #{name}"
+    end
+    puts waiting
+  end
 end
